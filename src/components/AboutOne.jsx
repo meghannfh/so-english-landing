c305b2fe6ae503daaff65aa455f310bbc9dfa9db
meghann <meghannfh@gmail.com>
2023-01-30 20:34:17 -0600

    about section 2

--- src/components/AboutOne.jsx
+++ src/components/AboutOne.jsx
@@ -8,10 +8,10 @@ export default function AboutOne(){
                 <h1 className="text-4xl font-bold uppercase absolute top-10 left-7 p-3 bg-white md:text-5xl md:top-24 md:left-36 lg:text-7xl"><span className="italic">Who is</span> So?</h1>
             </div>
 
-            <div className='px-6 py-24 md:px-36 lg:px-80'>
+            <div className='px-6 py-24 md:px-24 lg:px-56'>
                 <h2 className="text-5xl md:text-3xl font-semibold text-slate-900">About</h2>
-                <h3 className="mt-6 font-semibold leading-relaxed md:leading-loose text-4xl md:text-2xl text-slate-600">私は、友達や生徒や他の英語の先生方から「ペラペラだね」と言っていただくことはたまにありますが、自分自身をそのように思ったことは一度もありません。</h3>
-                <p className="mt-6 text-2xl leading-relaxed md:leading-loose md:text-lg font-semibold text-slate-600">それでも、どのようにして私が今のように英語を話せるようになり、海外に住み友達を作り、夜な夜なお酒を飲みながら政治の話や恋の話をし、ホテルや空港や市役所でトラブルがあっても話し合って解決をできるようになったかを少しお話したいと思います。英語と直接は関係のないことでも、英語学習のヒントになることがあるかもしれませんので、よければ読んでみてくださいね。</p>
+                <h3 className="mt-6 font-semibold leading-relaxed md:leading-loose text-4xl md:text-2xl text-sky-600">私は、友達や生徒や他の英語の先生方から「ペラペラだね」と言っていただくことはたまにありますが、自分自身をそのように思ったことは一度もありません。</h3>
+                <p className="mt-6 text-2xl leading-relaxed md:leading-loose md:text-lg font-medium text-slate-900">それでも、どのようにして私が今のように英語を話せるようになり、海外に住み友達を作り、夜な夜なお酒を飲みながら政治の話や恋の話をし、ホテルや空港や市役所でトラブルがあっても話し合って解決をできるようになったかを少しお話したいと思います。英語と直接は関係のないことでも、英語学習のヒントになることがあるかもしれませんので、よければ読んでみてくださいね。</p>
             </div>
         </div>
     )
