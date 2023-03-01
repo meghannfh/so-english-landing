09a0fe6bab42ebe2bc1327a357e5d9521919072b
meghann <meghannfh@gmail.com>
2023-03-01 13:23:32 -0600

    aligned menu options for small screens

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,7 @@
 export default function Menu(){
     return(
         <div>
-            <ul className="flex flex-col gap-3 md:gap-10 md:flex-row md:h-20 bg-slate-900 text-white justify-center lg:text-2xl">
+            <ul className="flex flex-col items-center pt-10 pb-2 gap-3 md:gap-10 md:flex-row md:h-20 bg-slate-900 text-white md:justify-center lg:text-2xl">
                 <li>ポッドキャスト</li>
                 <li>アバウト</li>
                 <li>アバウト</li>
